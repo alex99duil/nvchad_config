a7bf76191f34b7e900d3889235f28c6c066b2c2f
alex <alex99duil@gmail.com>
2024-05-12 13:42:10 +0300

    add support for python, c/c++, rust

--- lua/configs/conform.lua
+++ lua/configs/conform.lua
@@ -1,15 +1,20 @@
 local options = {
-  formatters_by_ft = {
-    lua = { "stylua" },
-    -- css = { "prettier" },
-    -- html = { "prettier" },
-  },
+	formatters_by_ft = {
+		lua = { "stylua" },
+		-- css = { "prettier" },
+		-- html = { "prettier" },
 
-  -- format_on_save = {
-  --   -- These options will be passed to conform.format()
-  --   timeout_ms = 500,
-  --   lsp_fallback = true,
-  -- },
+		json = { "clang_format" },
+		python = { "ruff_fix", "ruff_format" },
+		cpp = { "clang_format" },
+		rust = { "rustfmt" },
+	},
+
+	-- format_on_save = {
+	--   -- These options will be passed to conform.format()
+	--   timeout_ms = 500,
+	--   lsp_fallback = true,
+	-- },
 }
 
 require("conform").setup(options)
