3e396b15762d5194805335e75f7fe481cb016fc0
alex <alex99duil@gmail.com>
2024-05-12 16:42:47 +0300

    change theme, add dashboard header

--- lua/chadrc.lua
+++ lua/chadrc.lua
@@ -1,12 +1,28 @@
--- This file  needs to have same structure as nvconfig.lua 
+-- This file  needs to have same structure as nvconfig.lua
 -- https://github.com/NvChad/NvChad/blob/v2.5/lua/nvconfig.lua
 
 ---@type ChadrcConfig
 local M = {}
 
 M.ui = {
-	theme = "onedark",
+	theme = "tokyodark",
+	theme_toggle = { "catppuccin", "tokyodark" },
 
+	nvdash = {
+		load_on_startup = true,
+		header = {
+			"                                                       ",
+			"                                                       ",
+			" ███╗   ██╗ ███████╗ ██████╗  ██╗   ██╗ ██╗ ███╗   ███╗",
+			" ████╗  ██║ ██╔════╝██╔═══██╗ ██║   ██║ ██║ ████╗ ████║",
+			" ██╔██╗ ██║ █████╗  ██║   ██║ ██║   ██║ ██║ ██╔████╔██║",
+			" ██║╚██╗██║ ██╔══╝  ██║   ██║ ╚██╗ ██╔╝ ██║ ██║╚██╔╝██║",
+			" ██║ ╚████║ ███████╗╚██████╔╝  ╚████╔╝  ██║ ██║ ╚═╝ ██║",
+			" ╚═╝  ╚═══╝ ╚══════╝ ╚═════╝    ╚═══╝   ╚═╝ ╚═╝     ╚═╝",
+			"                                                       ",
+			"                                                       ",
+		},
+	},
 	-- hl_override = {
 	-- 	Comment = { italic = true },
 	-- 	["@comment"] = { italic = true },
