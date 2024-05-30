ed8bedeb22d1f449ec0f8b37049eae01e53abf15
alex <alex99duil@gmail.com>
2024-05-30 18:20:10 +0300

    feat: restore cursor position

--- init.lua
+++ init.lua
@@ -37,3 +37,21 @@ require "nvchad.autocmds"
 vim.schedule(function()
   require "mappings"
 end)
+
+local autocmd = vim.api.nvim_create_autocmd
+
+autocmd("BufReadPost", {
+  pattern = "*",
+  callback = function()
+    local line = vim.fn.line "'\""
+    if
+      line > 1
+      and line <= vim.fn.line "$"
+      and vim.bo.filetype ~= "commit"
+      and vim.fn.index({ "xxd", "gitrebase" }, vim.bo.filetype) == -1
+    then
+      vim.cmd 'normal! g`"'
+    end
+  end,
+})
+
