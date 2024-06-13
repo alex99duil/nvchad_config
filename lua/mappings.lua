66277df1b4be32a79415f6e863bf88de99f82656
alex <alex99duil@gmail.com>
2024-06-13 22:41:07 +0300

    add keybinds for moving line above and below

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -7,4 +7,6 @@ local map = vim.keymap.set
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 
+map("n", "<C-j>", ":m +1<CR>")
+map("n", "<C-k>", ":m -2<CR>")
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
