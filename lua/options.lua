c96c7f3075eb73a789f4859c8b71af4c49c48b98
alex <alex99duil@gmail.com>
2024-06-14 10:05:59 +0300

    add folding

--- lua/options.lua
+++ lua/options.lua
@@ -2,5 +2,9 @@ require "nvchad.options"
 
 -- add yours here!
 
--- local o = vim.o
+local o = vim.o
+o.foldlevel=20
+o.foldmethod = "expr"
+o.foldexpr = "nvim_treesitter#foldexpr()"
+-- o.nofoldenable = true
 -- o.cursorlineopt ='both' -- to enable cursorline!
